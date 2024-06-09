cb7444ec2ff2b9893fbdd998265222ff488dc730
Владислав <robert_trebor@list.ru>
2024-06-09 16:54:46 +0300

    Homework redone

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,17 @@ public class Main {
         int clientOS = 0;
         int releaseYear = 2024;
         printRequiredAppVersion(clientOS, releaseYear);
-        int deliveryDistance = 95;
-        int deliveryDays = 1;
-        calculateDeliveryDays(deliveryDistance, deliveryDays);
+        int deliveryDistance = 101;
+        int deliveryDay = calculateDeliveryDays(deliveryDistance);
+        if (deliveryDay > 3) {
+            System.out.println("Доставки нет.");
+        } else {
+            System.out.println("Потребуется для доставки дней: " + deliveryDay);
+        }
     }
 
     public static void printIsLeapYear(int year) {
-        if (year > 1584 && (year % 4 == 0 || year % 100 != 0 && year % 400 == 0)) {
+        if (year > 1584 && (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0))) {
             System.out.printf("«%s  год — високосный год».%n", year);
         } else {
             System.out.printf("«%s  год — невисокосный год».%n", year);
@@ -32,20 +36,18 @@ public class Main {
         }
     }
 
-    public static int calculateDeliveryDays(int deliveryDistance, int deliveryDays) {
-        if (deliveryDistance < 20) {
-            System.out.println("Потребуется дней: " + deliveryDays + ".");
-        } else if (deliveryDistance < 60) {
-            deliveryDays++;
-            System.out.println("Потребуется дней: " + deliveryDays + ".");
-        } else if (deliveryDistance < 100) {
-            deliveryDays += 2;
-            System.out.println("Потребуется дней: " + deliveryDays + ".");
-        } else {
-            System.out.println("Доставки нет.");
-            return -1;
+    public static int calculateDeliveryDays(int deliveryDistance) {
+        int deliveryDay = 1;
+        if (deliveryDistance > 20) {
+            deliveryDay++;
         }
-        return deliveryDays;
+        if (deliveryDistance > 60) {
+            deliveryDay++;
+        }
+        if (deliveryDistance > 100) {
+            deliveryDay++;
+        }
+        return deliveryDay;
     }
 
 }
